ab0b9d3eaabf8aea63a48757975f8ce58d095936
ggjj321 <10370324lsjh@gmail.com>
2021-08-24 15:42:53 +0800

    google map done

--- app/src/main/java/com/example/keras_to_android_sample/Main3Activity.java
+++ app/src/main/java/com/example/keras_to_android_sample/Main3Activity.java
@@ -5,6 +5,7 @@ import static android.view.View.*;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,6 +31,7 @@ public class Main3Activity extends AppCompatActivity {
     TextView t01,t02,txt1;
     Thread th;
     String location;
+    String foodName;
     String te01,te02;
 
     @Override
@@ -39,7 +41,6 @@ public class Main3Activity extends AppCompatActivity {
 
         buttonMap = (Button) findViewById(R.id.button1);
         buttonRecipe = (Button) findViewById(R.id.button2);
-        buttonRecommd = (Button) findViewById(R.id.button3);
 
 
         //傳送資料到Activity3
@@ -55,13 +56,10 @@ public class Main3Activity extends AppCompatActivity {
             //跳轉食譜
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent();
-                Bundle bundle = new Bundle();
-                bundle.putString("message", urlRecipe);
-                intent.putExtras(bundle);
-                intent.setClass(Main3Activity.this, Main4Activity.class);
-                //跳轉到Main3Activity
-                startActivity(intent);
+                Uri gmmIntentUri = Uri.parse("geo:0,0?q=" + Uri.encode(foodName));
+                Intent mapIntent = new Intent(android.content.Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
             }
         });
 
@@ -79,19 +77,6 @@ public class Main3Activity extends AppCompatActivity {
             }
         });
 
-        buttonRecommd.setOnClickListener(new OnClickListener() {
-            //跳轉食譜
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                Bundle bundle = new Bundle();
-                bundle.putString("message", urlRecipe);
-                intent.putExtras(bundle);
-                intent.setClass(Main3Activity.this, Main4Activity.class);
-                //跳轉到Main3Activity
-                startActivity(intent);
-            }
-        });
     }
     private Runnable catch_data = new Runnable(){
         public void run(){
@@ -100,25 +85,54 @@ public class Main3Activity extends AppCompatActivity {
                 switch (location)
                 {
                     case "apple_pie" :
+                        foodName = "蘋果派";
                         //recipe
                         urlRecipe = "https://a9556330.pixnet.net/blog/post/120768336-apple-pie";
                         urlRecommd = "https://ifoodie.tw/explore/list/%E8%98%8B%E6%9E%9C%E6%B4%BE?sortby=popular";
                         break;
 
                     case "baby_back_ribs" :
+                        foodName = "美式烤肋排";
                         //recipe
                         urlRecipe = "https://icook.tw/recipes/320467";
                         urlRecommd = "https://ifoodie.tw/explore/list/%E7%83%A4%E8%82%8B%E6%8E%92?sortby=popular";
                         break;
 
-                   case "bawan" :
-
+                    case "bawan" :
+                       foodName = "肉圓";
                        //recipe
                        urlRecipe = "https://icook.tw/recipes/193513";
-                       urlRecommd = "https://ifoodie.tw/explore/list/%E8%82%89%E5%9C%93?sortby=popular";
                        break;
 
+                    case "beef_carpaccio" :
+                        foodName = "beef_carpaccio";
+                        //recipe
+                        urlRecipe = "https://www.japancentre.com/zh/recipes/1318-beef-tataki";
+                        break;
 
+                    case "beef_noodles" :
+                        foodName = "beef_noodles";
+                        //recipe
+                        urlRecipe = "https://www.how-living.com/p/15335/%e6%af%94%e5%86%a0%e8%bb%8d%e5%90%8d%e5%ba%97%e9%82%84%e5%a5%bd%e5%90%83%ef%bc%81%e8%bf%b7%e4%ba%ba%e9%a3%84%e9%a6%99%e3%80%90%e7%95%aa%e8%8c%84%e7%b4%85%e7%87%92%e7%89%9b%e8%82%89%e9%ba%b5%e3%80%91";
+                        break;
+
+                    case "beef_tartare" :
+                        foodName = "beef_tartare";
+                        //recipe
+                        urlRecipe = "https://icook.tw/recipes/207010";
+                        break;
+
+                    case "bibimbap" :
+                        foodName = "bibimbap";
+                        //recipe
+                        urlRecipe = "https://icook.tw/recipes/328341";
+                        break;
+
+                    case "bitter_melon_with_salted_eggs" :
+                        foodName = "bitter_melon_with_salted_eggs";
+                        //recipe
+                        urlRecipe = "https://icook.tw/recipes/316807";
+                        break;
                 }
 
             }
---
+++ app/src/main/java/com/example/keras_to_android_sample/MapsActivity.java
@@ -0,0 +1,98 @@
+package com.example.keras_to_android_sample;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.keras_to_android_sample.databinding.ActivityMapsBinding;
+
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+    // 移動地圖到參數指定的位置
+    private void moveMap(LatLng place) {
+        // 建立地圖攝影機的位置物件
+        CameraPosition cameraPosition =
+                new CameraPosition.Builder()
+                        .target(place)
+                        .zoom(17)
+                        .build();
+
+        // 使用動畫的效果移動地圖
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+    private void setUpMap() {
+        // 刪除原來預設的內容
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+
+        // 建立位置的座標物件
+        LatLng place = new LatLng(25.033408, 121.564099);
+        // 移動地圖
+        moveMap(place);
+
+        // 加入地圖標記
+        addMarker(place, "Hello!", " Google Maps v2!");
+    }
+
+    // 在地圖加入指定位置與標題的標記
+    private void addMarker(LatLng place, String title, String snippet) {
+        BitmapDescriptor icon =
+                BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(place)
+                .title(title)
+                .snippet(snippet)
+                .icon(icon);
+
+        mMap.addMarker(markerOptions);
+    }
+
+}
+
+
+
